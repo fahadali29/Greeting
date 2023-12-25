146831cade5b7aeedc2a2c62ac7a8e1753c9fba5
fhd <fahadali2973747@gmail.com>
2023-12-25 13:24:54 +0500

    finl

--- app/src/main/java/com/example/greeting/MainActivity.java
+++ app/src/main/java/com/example/greeting/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.greeting;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editText;
+    Button mybtn;
+    TextView title;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.edittext);
+        mybtn = findViewById(R.id.button);
+        title = findViewById(R.id.text);
+        mybtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String input = editText.getText().toString();
+                Toast.makeText(MainActivity.this, "Welcome" + input, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
